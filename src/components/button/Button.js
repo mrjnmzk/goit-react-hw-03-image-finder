7a6cb06967f6a4f5614c8a125a7eacdeb86da75a
marian muzyka <mrjnmzk@gmail.com>
2020-08-29 19:52:03 +0300

    xxc

---
+++ src/components/button/Button.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
+
+const Button = ({ onClick }) => {
+  return (
+    <div>
+      <button type="button" onClick={onClick} className={styles.Button}>
+        I Whant More
+      </button>
+    </div>
+  );
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default Button;
